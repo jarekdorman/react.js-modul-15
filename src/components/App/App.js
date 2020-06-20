2950b3b0bf58cc5f5e9b414705da5cb931082591
Jaroslaw Jay Dorman <jay@jaydorman.com>
2020-06-20 12:15:50 -0400

    add search/filter function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import styles from './App.scss';
 import List from '../List/listContainer.js';
 // import { pageContents, listData } from '../../data/dataStore';
 import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer';
 
 class App extends React.Component {
   static propTypes = {
@@ -17,6 +18,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search />
         {lists.map((listData) => (
           <List key={listData.id} {...listData} />
         ))}
